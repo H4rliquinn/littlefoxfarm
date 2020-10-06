63ba2e9982068d2d53ecfab71e431fcf84991bf4
Jason Sonnichsen <Harliquinn@gmail.com>
2020-10-06 12:54:46 -0400

    first commit

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,22 @@
+import { Link, routes } from '@redwoodjs/router'
+
+const HomePage = () => {
+  return (
+    <>
+      <header>
+        <img src="#" alt="Logo" />
+      </header>
+      <content>
+        <p>We are excited to be accepting new boarders starting in November.</p>
+        <p>
+          Inquiries can be directed to Kim Rother at:
+          <br />
+          <a href="mailto:skylandmanor@hotmail.com">skylandmanor@hotmail.com</a>
+          <p>Cell: 845-559-4899</p>
+        </p>
+      </content>
+    </>
+  )
+}
+
+export default HomePage
