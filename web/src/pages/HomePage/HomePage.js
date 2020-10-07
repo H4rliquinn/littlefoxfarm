b7ed3efe22b9224e9730f79d6870c21231131b3b
Jason Sonnichsen <Harliquinn@gmail.com>
2020-10-07 17:29:57 -0400

    Updated page with styles and a picture

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,12 +1,34 @@
-import { Link, routes } from '@redwoodjs/router'
-
+// import { Link, routes } from '@redwoodjs/router'
+import img from '../../img/cac9558f139212d6e1df03aaeaa75a1d230a833a_big.jpg'
 const HomePage = () => {
   return (
     <>
-      <header>
+      <header className="header">
         <img src="#" alt="Logo" />
+        <div>LITTLE FOX FARM</div>
       </header>
       <content>
+        <img src={img} alt="Farm Overview" width="400px" />
+        <description>
+          <p>
+            Little Fox Farm is a beautiful equestrian estate spanning over 25+
+            acres approx. 1 hr from New York City. The barn has 17 stalls,
+            indoor and outdoor rings. 4 paddocks w/4 board fence with electric.
+          </p>
+          <p>
+            72&apos;x160&apos; indoor ring w/viewing room, heated tack room, hot
+            or cold wash down, bath room and laundry.
+          </p>
+          <p>
+            Outdoor ring measures approx. 100&apos;x200&apos; w/sand footing, 2
+            gates and automatic sprinkler.
+          </p>
+          <p>
+            WiFi and smoke detectors in barns. Spectacular farmland with the
+            Appalachian Trail nearby. 10 minutes from NJ Fairgrounds. Beautiful
+            fruit trees adorn the property. A horse lover’s paradise!
+          </p>
+        </description>
         <p>We are excited to be accepting new boarders starting in November.</p>
         <p>
           Inquiries can be directed to Kim Rother at:
