c88dc296163e4f0d546e830171a5c304e19f1532
Jason Sonnichsen <Harliquinn@gmail.com>
2020-10-12 21:06:24 -0400

    Removed unnecessary images from the project using adjusted ones

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,23 +1,26 @@
 // import { Link, routes } from '@redwoodjs/router'
-import logo from 'src/img/logo.png'
 
 const HomePage = () => {
   return (
     <div className="body">
       <header className="header">
         <div className="logoBox">
-          <img src={logo} alt="Logo" className="logo" />
+          <img src={require('src/img/logo.png')} alt="Logo" className="logo" />
         </div>
         <div className="titleBox">
           <h1>LITTLE FOX FARM</h1>
         </div>
       </header>
       <main>
-        <p className="infoBox">
-          Little Fox Farm is a beautiful equestrian estate spanning over 25+
-          acres approx. 1 hr from New York City. The barn has 17 stalls, indoor
-          and outdoor rings. 4 paddocks w/4 board fence with electric.
-        </p>
+        <img src={require('src/img/aisle.jpg')} alt="aisle" />
+        <div className="infoBox">
+          <p>
+            Little Fox Farm is a beautiful equestrian estate spanning over 25+
+            acres approx. 1 hr from New York City. The barn has 17 stalls,
+            indoor and outdoor rings. 4 paddocks w/4 board fence with electric.
+          </p>
+        </div>
+        <img src={require('src/img/indoor.jpg')} alt="indoor" />
         <p className="infoBox">
           72&apos;x160&apos; indoor ring w/viewing room, heated tack room, hot
           or cold wash down, bath room and laundry.
@@ -31,6 +34,7 @@ const HomePage = () => {
           Appalachian Trail nearby. 10 minutes from NJ Fairgrounds. Beautiful
           fruit trees adorn the property. A horse lover’s paradise!
         </p>
+        <img src={require('src/img/outdoor.jpg')} alt="outdoor" />
         <p className="infoBox">
           We are excited to be accepting new boarders starting in November.
         </p>
